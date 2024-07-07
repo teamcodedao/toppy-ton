d829adbec53e61c8e95e8519572bc3ce0352b31d
Nghiep <me@nghiep.dev>
2024-07-07 16:31:29 +0700

    Clean up

---
+++ next-app.d.ts
@@ -0,0 +1,15 @@
+export {};
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NEXT_PUBLIC_METADATABASE_URL: string;
+      NEXT_PUBLIC_COIN_URL: string;
+      NEXT_PUBLIC_COIN_ADDRESS: string;
+      NEXT_PUBLIC_TWITTER_URL: string;
+      NEXT_PUBLIC_TELEGRAM_URL: string;
+      NEXT_PUBLIC_DEX_URL: string;
+      NEXT_PUBLIC_DEXS_URL: string;
+    }
+  }
+}
