18a9298ddfaf133aea9801a54a4900978735d8e2
Nghiep <me@nghiep.dev>
2024-07-07 18:12:19 +0700

    Add socials

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 
+import Socials from './socials';
+
 import toppy1Img from './toppy-1.png';
 import toppy2Img from './toppy-2.svg';
 import grassImg from './grass.svg';
@@ -9,7 +11,7 @@ export default function Home() {
     <main className='min-h-screen'>
       {/* Section */}
       <div>
-        <div className='relative pt-20 mx-auto max-w-screen-2xl px-5'>
+        <div className='relative pt-12 sm:pt-20 mx-auto max-w-screen-2xl px-5'>
           <h1 className='text-shadow text-center text-8xl sm:text-9xl uppercase text-primary [-webkit-text-stroke:1px_#000] relative z-10'>
             $TOPPY
           </h1>
@@ -21,24 +23,25 @@ export default function Home() {
             className='mt-28 w-[880px] max-w-[50%] 2xl:max-w-full object-contain relative z-20'
           />
           <div className='absolute inset-0 flex justify-end items-end'>
-            <div>
+            <div className='relative'>
               <Image
                 src={toppy2Img}
                 priority
                 alt=''
                 className='w-[calc(50vw+80px)] 2xl:w-auto object-cover object-bottom'
               />
+              <Socials />
             </div>
           </div>
           <img
             src='/planes.svg'
             alt=''
-            className='absolute top-0 left-0 img-planes z-0'
+            className='absolute -top-5 sm:top-0 left-0 img-planes z-0'
           />
           <img
             src='/planes.svg'
             alt=''
-            className='absolute top-0 right-0 scale-x-[-1] img-planes z-0'
+            className='absolute -top-5 sm:top-0 right-0 scale-x-[-1] img-planes z-0'
           />
         </div>
         <Image
---
+++ app/socials.tsx
@@ -0,0 +1,51 @@
+import clsx from 'clsx';
+
+export default function Socials() {
+  return (
+    <div
+      className={clsx(
+        'absolute inset-0 z-20 -mt-36 flex flex-col items-end -space-y-10 sm:-mt-44 xl:-mt-56 xl:space-y-0 2xl:max-w-[95%]',
+        '*:multi-[scale-50;origin-bottom] sm:*:scale-75 2xl:*:scale-100',
+        '[&_a]:relative',
+        '[&>*_img:last-child]:multi-[absolute;left-1/2;top-1/2;-translate-x-1/2;-translate-y-1/2;object-contain]'
+      )}
+    >
+      <a
+        href={process.env.NEXT_PUBLIC_DEXS_URL}
+        target='_dexs'
+        className='self-center'
+      >
+        <img src='/socials/dexs-bg.svg' alt='' />
+        <img
+          src='/socials/dexs.png'
+          alt=''
+          className='size-[60px] rounded-full'
+        />
+      </a>
+      <div className='flex w-full justify-center gap-x-10 xl:mt-10'>
+        <a href={process.env.NEXT_PUBLIC_TWITTER_URL} target='_x'>
+          <img src='/socials/x-bg.svg' alt='' />
+          <img src='/socials/x.png' alt='' className='size-[60px]' />
+        </a>
+        <a href={process.env.NEXT_PUBLIC_TELEGRAM_URL} target='_tele'>
+          <img src='/socials/tele-bg.svg' alt='' />
+          <img
+            src='/socials/tele.png'
+            alt=''
+            className='text-shadow -mt-4 size-[60px]'
+          />
+        </a>
+      </div>
+      <div className='flex justify-end'>
+        <a href={process.env.NEXT_PUBLIC_DEX_URL} target='_dex'>
+          <img src='/socials/dex-bg.svg' alt='' />
+          <img
+            src='/socials/dex.png'
+            alt=''
+            className='text-shadow size-[60px]'
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
